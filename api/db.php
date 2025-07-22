0e5692a78f3c1ac9e5330013b2fb70f051dc61d4
Rita <qazasd1011@gmail.com>
2025-07-22 13:12:35 +0800

    新增海報功能

--- api/db.php
+++ api/db.php
@@ -2,3 +2,145 @@
 session_start();
 
 date_default_timezone_set("Asia/Taipei");
+
+function dd($data){
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+function to($url){
+    header("location:".$url);
+}
+
+function q($sql){
+    $dsn="mysql:host=localhost;dbname=db02;charset=utf8";
+    $pdo=new PDO($dsn,'root','');
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
+
+class DB{
+    private $table;
+    private $dsn="mysql:host=localhost;dbname=db02;charset=utf8";
+    private $pdo;
+
+    function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+    }
+
+    function all(...$arg){
+        $sql="select * from $this->table ";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                $tmp=$this->array2sql($arg[0]);
+                $sql .=" where ".join(" and ",$tmp);
+            }else{
+                $sql .=$arg[0];
+            }
+        }
+
+        if(isset($arg[1])){
+            $sql .=$arg[1];
+        }
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function count(...$arg){
+        $sql="select count(*) from $this->table ";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                $tmp=$this->array2sql($arg[0]);
+                $sql .=" where ".join(" and ",$tmp);
+            }else{
+                $sql .=$arg[0];
+            }
+        }
+
+        if(isset($arg[1])){
+            $sql .=$arg[1];
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    function sum($cols,...$arg){
+        $sql="select sum($cols) from $this->table ";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                $tmp=$this->array2sql($arg[0]);
+                $sql .=" where ".join(" and ",$tmp);
+            }else{
+                $sql .=$arg[0];
+            }
+        }
+
+        if(isset($arg[1])){
+            $sql .=$arg[1];
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    function max($cols,...$arg){
+        $sql="select max($cols) from $this->table ";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                $tmp=$this->array2sql($arg[0]);
+                $sql .=" where ".join(" and ",$tmp);
+            }else{
+                $sql .=$arg[0];
+            }
+        }
+
+        if(isset($arg[1])){
+            $sql .=$arg[1];
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+
+
+    function find($id){
+          $sql="select * from $this->table ";
+            if(is_array($id)){
+                $tmp=$this->array2sql($id);
+                $sql .=" where ".join(" and ",$tmp);
+            }else{
+                $sql .=" where `id`='$id'";
+            }
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    function save($array){
+        if(isset($array['id'])){
+            $tmp=$this->array2sql($array);
+            $sql="update $this->table set ";
+            $sql .=join(" , ",$tmp)." where `id`='{$array['id']}'";
+        }else{
+            $cols=join("`,`",array_keys($array));
+            $values=join("','",$array);
+            $sql="insert into $this->table (`$cols`) values('$values')";
+        }
+        return $this->pdo->exec($sql);
+    }
+
+    function del($id){
+           $sql="delete from $this->table ";
+            if(is_array($id)){
+                $tmp=$this->array2sql($id);
+                $sql .=" where ".join(" and ",$tmp);
+            }else{
+                $sql .=" where `id`='$id'";
+            }
+        return $this->pdo->exec($sql);
+    }
+
+    function array2sql($array){
+        $tmp=[];
+        foreach($array as $key => $value){
+            $tmp[]="`$key`='$value'";
+        }
+        return $tmp;
+    }
+}
+
+$Poster=new DB('posters');
--- backend/poster.php
+++ backend/poster.php
@@ -1,1 +1,21 @@
-poster
+<div style="height:320px">
+<h3 class="ct" style="margin:0;">預告片清單</h3>
+</div>
+<hr>
+<div style="height:160px">
+<h3 class="ct" style="margin:0;">新增預告片海報</h3>
+<form action="./api/add_poster.php" method="post" enctype="multipart/form-data">
+    <table style="margin:auto;">
+        <tr>
+            <td>預告片海報:</td>
+            <td><input type="file" name="img" id="img"></td>
+            <td>預告片片名:</td>
+            <td><input type="text" name="name" id="name"></td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="submit" value="新增">
+        <input type="reset" value="重置">
+    </div>
+</form>
+</div>
