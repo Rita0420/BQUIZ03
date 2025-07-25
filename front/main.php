106d8fade467a42d77534e7d2a53eb5a373b8aaf
Rita <qazasd1011@gmail.com>
2025-07-25 13:16:06 +0800

    完成前端預告片顯示

--- front/main.php
+++ front/main.php
@@ -4,7 +4,7 @@
     height: 240px;
     /* background-color: lightblue; */
     margin: auto;
-    overflow:hidden;
+    overflow: hidden;
     position: relative;
 }
 
@@ -12,43 +12,58 @@
     width: 320px;
     height: 120px;
     background-color: lightpink;
+    display: flex;
+    overflow: hidden;
 }
 
-.left{
+.left {
     width: 0;
-    height:0;
-    border-left:0px solid black;
-    border-right:30px solid black;
-    border-top:20px solid transparent;
-    border-bottom:20px solid transparent;
+    height: 0;
+    border-left: 0px solid black;
+    border-right: 30px solid black;
+    border-top: 20px solid transparent;
+    border-bottom: 20px solid transparent;
 }
 
-.right{
+.right {
     width: 0;
-    height:0;
-    border-left:30px solid black;
-    border-right:0px solid black;
-    border-top:20px solid transparent;
-    border-bottom:20px solid transparent;
+    height: 0;
+    border-left: 30px solid black;
+    border-right: 0px solid black;
+    border-top: 20px solid transparent;
+    border-bottom: 20px solid transparent;
 }
 
-.controls{
-    display:flex;
-    align-items:center;
-    justify-content:space-around;
+.controls {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
 }
 
-.poster img{
-    width:200px;
-    height:220px;
+.poster img {
+    width: 200px;
+    height: 220px;
 }
 
-.poster{
-    width:210px;
-    height:240px;
+.poster {
+    width: 210px;
+    height: 240px;
     position: absolute;
-    text-align:center;
-    display:none;
+    text-align: center;
+    display: none;
+}
+
+.poster-btn {
+    width: 80px;
+    height: 100px;
+    /* display: inline-block; */
+    flex-shrink: 0;
+    position: relative;
+}
+
+.poster-btn img {
+    width: 70px;
+    height: 90px;
 }
 </style>
 <?php
@@ -60,24 +75,32 @@ $posters=$Poster->all(['sh'=>1]," order by `rank` ");
     <div class="rb tab" style="width:95%;">
         <div id="abgne-block-20111227">
             <div class="lists">
-            <?php foreach($posters as $poster):?>
-                <div class="poster">
+                <?php foreach($posters as $poster):?>
+                <div class="poster" data-ani="<?=$poster['ani'];?>" data-id="<?=$poster['id'];?>">
                     <img src="./images/<?=$poster['img'];?>" alt="">
                     <div><?=$poster['name'];?></div>
                 </div>
-            <?php endforeach;?>
+                <?php endforeach;?>
 
             </div>
             <div class="controls">
                 <div class="left"></div>
                 <div class="btns">
-
+                    <?php
+                    foreach($posters as $key => $poster):
+                    ?>
+                    <div class="ct poster-btn" data-ani="<?=$poster['ani'];?>" data-id="<?=$poster['id'];?>">
+                        <img src="./images/<?=$poster['img'];?>" alt="">
+                        <div><?=$poster['name'];?></div>
+                    </div>
+                    <?php endforeach;?>
                 </div>
-                <div class="right"></div>
 
+                <div class="right"></div>
             </div>
         </div>
     </div>
+</div>
 </div>
 <div class="half">
     <h1>院線片清單</h1>
@@ -92,5 +115,85 @@ $posters=$Poster->all(['sh'=>1]," order by `rank` ");
 </div>
 
 <script>
-    $('.poster').eq(0).show();
+let rank = 0;
+$('.poster').eq(rank).show();
+
+let slider = setInterval(() => {
+
+    animater()
+
+}, 2000);
+
+$('.btns').hover(
+    function () {
+        clearInterval(slider);
+        
+    }, function () {
+        slider=setInterval(() => {
+            animater();
+        }, 2000);
+    }
+);
+
+$('.poster-btn').on('click',function(){
+    let idx=$(this).index();
+    animater(idx);
+})
+
+function animater(r) {
+    let now = $('.poster:visible');
+    if(r==undefined){
+        rank++;
+        if (rank > $('.poster').length - 1) {
+            rank = 0;
+        }
+    }else{
+        rank=r;
+    }
+    let next = $('.poster').eq(rank);
+    let ani = $(now).data('ani');
+    console.log(ani);
+
+
+    switch (ani) {
+        case 1:
+            $(now).fadeOut(1000);
+            $(next).fadeIn(1000);
+            break;
+        case 2:
+            $(now).hide(1000);
+            $(next).show(1000);
+            break;
+        case 3:
+            $(now).slideUp(1000);
+            $(next).slideDown(1000);
+            break;
+
+        default:
+            break;
+    }
+}
+
+let p=0;
+$('.left,.right').on('click',function(){
+    let arrow=$(this).attr('class');
+    switch (arrow) {
+        case 'left':
+            if(p > 0){
+                p--;
+            }
+            break;
+
+        case 'right':
+            if(p < $('.poster-btn').length-4){
+                p++;
+            }
+            break;
+    
+        default:
+            break;
+    }
+
+    $('.poster-btn').animate({right:p*80},500)
+})
 </script>
