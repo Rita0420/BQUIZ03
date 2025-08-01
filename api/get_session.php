2873005352d18244643160f3a2d7c383836b3b26
Rita <qazasd1011@gmail.com>
2025-08-01 16:36:24 +0800

    訂單

--- api/get_session.php
+++ api/get_session.php
@@ -5,7 +5,7 @@ include_once "db.php";
 $id=$_GET['movieId'];
 $date=$_GET['date'];
 
-$movies=$Movies->find($id);
+$movie=$Movies->find($id);
 $ondate=strtotime($movies['ondate']);
 $today=strtotime(date('Y-m-d'));
 
@@ -18,7 +18,7 @@ if($date == date("Y-m-d") && $hr>13 ){
 
 
 for($i=$start;$i<5;$i++){
-    $remaining=20; //假設每場剩餘20個座位，20-已被訂走的座位
+    $remaining=20-$Order->sum('tickets',['movie'=>$movie['name'],'date'=>$movie['date'],'session'=>$sessStr[$i]]); //假設每場剩餘20個座位，20-已被訂走的座位
     echo "<option value='{$sessStr[$i]}'>";
     echo $sessStr[$i];
     echo "剩餘座位 ";
--- backend/order.php
+++ backend/order.php
@@ -1,1 +1,113 @@
-order
+<style>
+    .list-col{
+        display:flex;
+        justify-content:space-between;
+        margin-top:5px;
+    }
+
+    .list-col div{
+        width: calc( 98% / 7);
+        background:#ccc;
+    }
+    .list-data{
+        display:flex;
+        justify-content:space-between;
+        margin-top:5px;
+        align-items:center;
+    }
+
+    .list-data div{
+        width: calc( 98% / 7);
+    }
+
+    #items{
+        overflow:auto;
+        height:340px;
+    }
+</style>
+<h3 class="ct">訂單管理</h3>
+<div class="quick-del">
+    快速刪除:
+    <input type="radio" name="type" id="date" value="date" checked>依日期
+    <input type="text" name="date">
+    
+    <input type="radio" name="type" id="movie" value="movie">依電影
+    <select name="movie" id="">
+        <?php
+        $movies=q("select `movie` from `orders` group by `movie`");
+        foreach($movies as $movie){
+            echo "<option value='{$movie['movie']}'>{$movie['movie']}</option>";
+        }
+        ?>
+    </select>
+
+    <div class="list-col ct">
+        <div>訂單編號</div>
+        <div>電影名稱</div>
+        <div>日期</div>
+        <div>場次時間</div>
+        <div>訂購數量</div>
+        <div>訂購位置</div>
+        <div>操作</div>
+    </div>
+    <div id="items">
+        <?php
+        $orders=$Orders->all(" order by `no` desc");
+        foreach($orders as $order):
+        ?>
+        <div class="list-data ct">
+            <div><?=$order['no'];?></div>
+            <div><?=$order['movie'];?></div>
+            <div><?=$order['date'];?></div>
+            <div><?=$order['session'];?></div>
+            <div><?=$order['tickets'];?></div>
+            <div><?php
+            $seats=unserialize($order['seats']);
+            sort($seats);
+            foreach($seats as $seat){
+                
+                echo floor($seat/5)+1 ."排".($seat%5)+1 ."號<br>";
+               
+            }
+            
+            
+            ?></div>
+            <div><button class="btn-del" data-id="<?=$order['id'];?>">刪除</button></div>
+        </div>
+        <hr>
+        <?php endforeach;?>
+    </div>
+
+</div>
+
+<script>
+    $('.btn-del').on("click",function(){
+        let id=$(this).data('id');
+        console.log(id);
+        
+        if(confirm("確定要刪除這筆訂單嗎?")){
+            $.post("./api/del.php",{table:'Orders',id},()=>{
+                location.reload();
+            })
+        }
+    })
+
+    $(".quick-del").on("click",function(){
+        let type=$("input[name='type']:checked").val();
+        let data='';
+        switch(type){
+            case 'date':
+                data=$("#date").val();
+            break;
+            case 'movie':
+                data=$("#movie").val();
+            break;
+        }
+
+        if(confirm(`確定要刪除${data}的所有訂單嗎?`)){
+            $.post("./api/qDel.php",{type,date},(res){
+
+            })
+        }
+    })
+</script>
